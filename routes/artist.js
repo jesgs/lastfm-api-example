70f9d75f3d3aa9b114607a1db5e49affe36c42fb
Jess Green <ardath.ksheyna78@gmail.com>
2018-02-15 19:23:03 -0600

    Adding styling for all views

--- routes/artist.js
+++ routes/artist.js
@@ -6,10 +6,12 @@ artist.get('/:id', (req, res) => {
     const endpoint = '?method=artist.gettopalbums&format=json&user='
         + process.env.LASTFM_USER + "&api_key=" + process.env.LASTFM_API_KEY
         + "&autocorrect=1"
+        + "&limit=15"
         + "&artist=" + artist;
-    // res.status(200).render('artist', {artist: artist});
+
     client.get(endpoint).then((response) => {
         res.status(200).render('artist', {
+            debug: response.data,
             artist: response.data.topalbums['@attr'],
             albums: response.data.topalbums.album
         });
